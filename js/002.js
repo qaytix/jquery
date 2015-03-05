247a8608596ef87edab312e01e525415419373f4
qaytix <2210127029@qq.com>
2015-03-05 03:38:37 -0800

    Revert "upper"
    This reverts commit aa49ac6df35b6ed50ae84ee55eefda8923b8b92a.

--- js/002.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("p").css("color", "red").hover(function() {
-		$(this).css("background", "#FF9900").fadeIn("slow");
-	}, function() {
-		$(this).css("background", "green").fedeOut("slow");
-	});
-});
--- js/02.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("p").css("color","red").mouseover(function(){
-		$(this).css("color","#FF9900");
-	}).mouseout(function(){
-		$(this).css("color","green");
-	});
-});
